da545fd810c2654bb2985c31a5903108f0a83a21
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2018-06-01 16:43:40 +0530

    Gave the exam

--- src/com/sensei/ocaprep/StringBuilderTest.java
+++ src/com/sensei/ocaprep/StringBuilderTest.java
@@ -3,6 +3,11 @@ package com.sensei.ocaprep;
 public class StringBuilderTest {
 
 	public static void main(String[] args) {
+		String s = "CAFEBABE";
+		System.out.println( s.replace( 'A', 'X' ) );
+		System.out.println( s.replace( "A" , "X" ) );
+		System.out.println( s.replace( "BABE" , "DEAD" ) );
+		
 		StringBuilder sb = new StringBuilder( "Long1203021sfs gibberish numeric" );
 	}
 	
